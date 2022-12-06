771fd6d03e961526fa5e4875b3066afbe1859985
m-roberts <2947595+m-roberts@users.noreply.github.com>
2022-12-06 00:09:19 +0000

    Configure OMZ for container

--- bootstrap-vs-code-dev-container.sh
+++ bootstrap-vs-code-dev-container.sh
@@ -3,4 +3,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+apt-get update
+apt-get install -y zsh
+
+# Zsh
+chsh -s /usr/bin/zsh
+
+# Oh-my-zsh
+/bin/bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
