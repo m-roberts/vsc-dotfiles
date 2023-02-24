e232416bc3bf129cf6c99e6728d366bd5f9efc52
M. R <2947595+m-roberts@users.noreply.github.com>
2023-02-24 16:17:58 +0000

    Update bootstrap-vs-code-dev-container.sh

--- bootstrap-vs-code-dev-container.sh
+++ bootstrap-vs-code-dev-container.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 
 # Install zsh
 apt-get update
-apt-get install -y zsh
+apt-get install -y zsh less
 
 # Install Oh-My-Zsh
 /bin/bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
